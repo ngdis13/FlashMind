ff3f5a85851e2eeab130bc70b27b866dcbdc609c
ngdis13 <the.n_kocherovaaa@bk.ru>
2025-10-03 22:26:46 +0300

    Готовая база данных

--- app/schemas.py
+++ app/schemas.py
@@ -1,38 +1,37 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict
 
 
-class Topic(BaseModel):
+class TopicBase(BaseModel):
     """Базовая схема для темы
     Args:
         BaseModel (_type_): унаследованный класс BaseModel
     """
 
-    id: int
-    name: str = "Math"
+    name: str
     description: Optional[str]
 
 
-class TopicCreate(Topic):
+class TopicCreate(TopicBase):
     """Схема для создания новой темы"""
 
     pass
 
 
-class TopicUpdate(Topic):
+class TopicUpdate(BaseModel):
     """Схема для обновления текущей темы"""
 
-    name: Optional[str] = Field(example="Высшая математика")
-    description: Optional[str] = Field(example="Уравнения прямой")
+    name: Optional[str] = None
+    description: Optional[str] = None
 
 
-class TopicRead(Topic):
+class TopicRead(TopicBase):
     """Схема для чтения темы (ответа API). Включает ID и временные метки."""
 
-    id: int = Field(..., example=1)
-    created_at: datetime = Field(default_factory=datetime.now, example="2023-10-27T10:00:00.000Z")
-    updated_at: datetime = Field(default_factory=datetime.now, example="2023-10-27T10:00:00.000Z")
+    id: int
+    created_at: datetime
+    updated_at: datetime
 
     model_config = ConfigDict(from_attributes=True)
