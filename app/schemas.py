ff9e78438f2b1b70394cdcaffdce6a21a63b1291
ngdis13 <the.n_kocherovaaa@bk.ru>
2025-10-03 22:43:17 +0300

    Добавила схемы для карточек и тем

--- app/schemas.py
+++ app/schemas.py
@@ -1,14 +1,11 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel
 
 
 class TopicBase(BaseModel):
-    """Базовая схема для темы
-    Args:
-        BaseModel (_type_): унаследованный класс BaseModel
-    """
+    """Базовая схема для темы"""
 
     name: str
     description: Optional[str]
@@ -34,4 +31,42 @@ class TopicRead(TopicBase):
     created_at: datetime
     updated_at: datetime
 
-    model_config = ConfigDict(from_attributes=True)
+    class Config:
+        orm_mode = True
+
+
+class FlashcardBase(BaseModel):
+    """Базовая схема для карточки"""
+
+    question: str
+    answer: str
+    difficulty_level: int = 1
+
+
+class FlashcardCreate(FlashcardBase):
+    """Базовая схема для создания карточки"""
+
+    pass
+
+
+class FlashcardUpdate(BaseModel):
+    """Схема для обновления карточки"""
+
+    topic_id: Optional[int] = None
+    question: Optional[str] = None
+    answer: Optional[str] = None
+    difficulty_level: Optional[int] = None
+    last_reviewed_at: Optional[datetime] = None
+
+
+class FlashcardRead(FlashcardBase):
+    """Схема для чтения карточки"""
+
+    id: int
+    topic_id: int
+    last_reviewed_at: Optional[datetime] = None
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
